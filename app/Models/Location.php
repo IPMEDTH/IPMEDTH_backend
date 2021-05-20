dc68bf3db2cef1a7ee39572ad2c50d6162365a6f
Kiwikisser <amos.peperkamp@gmail.com>
2021-05-20 17:27:18 +0200

    Merge branch 'main' of https://github.com/IPMEDTH/IPMEDTH_backend into main

---
+++ app/Models/Location.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Location extends Model
+{
+    use HasFactory;
+
+    /**
+     * The name of the database table.
+     *
+     * @var string
+     */
+    protected $table = 'locations';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'date',
+        'start_time',
+        'end_time',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'date' => 'date',
+        'start_time' => 'time',
+        'end_time' => 'time'
+    ];
+}
