5384c50dc1bfd94ee237f161f9fbb3d542813197
Joeriubink <joeri.ubink@outlook.com>
2021-06-07 08:21:10 +0200

    updated some seedings in database + added pictures

--- database/seeders/LocationSeeder.php
+++ database/seeders/LocationSeeder.php
@@ -16,16 +16,30 @@ class LocationSeeder extends Seeder
     {
         Location::insert([
             'name' => '3D Printer',
-            'description' => 'Dit is een mooie 3D Printer',
+            'description' => '2 Ultimaker 3D printers',
             'amount' => 2,
-            'image_url' => 'https://picsum.photos/200/200'
+            'image_url' => 'https://api.ipmedth.meulen.dev/1.jpeg'
         ]);
 
         Location::insert([
             'name' => 'Lasersnijder',
-            'description' => 'Dit is een mooie lasersnijder',
+            'description' => 'Een lasersnijder in een aparte ruimte',
             'amount' => 1,
-            'image_url' => 'https://api.ipmedth.meulen.dev/1.jpeg'
+            'image_url' => 'https://api.ipmedth.meulen.dev/lasersnijder.jpg'
+        ]);
+
+        Location::insert([
+            'name' => 'Houtbewerkingslokaal',
+            'description' => 'Een plek om te werken in het houtbewerkingslokaal',
+            'amount' => 10,
+            'image_url' => 'https://api.ipmedth.meulen.dev/houtbewerking.jpeg'
+        ]);
+
+        Location::insert([
+            'name' => 'Werkplek',
+            'description' => 'Een werkplek om aan te werken, zonder apparaten',
+            'amount' => 30,
+            'image_url' => 'https://api.ipmedth.meulen.dev/werkplek.jpeg'
         ]);
     }
 }
