36002f8f59f9a859149c204b495f1134750ecd35
matthijsvdmeulen <matthijsvdmeulen@gmail.com>
2021-07-05 16:23:23 +0200

    add isadmin role to user model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('knowledge')->nullable();
             $table->string('available')->nullable();
+            $table->boolean('isadmin')->default(false);
             $table->string('password');
             $table->string('image_url')->nullable();
             $table->rememberToken();
